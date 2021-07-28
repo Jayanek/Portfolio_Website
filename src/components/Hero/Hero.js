5e103459d63e2daf9d9bdcbff70f37e90e5d247c
Jayanek <jayanekwije123@gmail.com>
2021-07-28 11:57:00 +0530

    Hero Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello, I am Jayanaka Wijethunga <br />
+        Welcome To My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      A Junior Software Engineer & DotNet Developer
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+</>
 );
 
 export default Hero;
